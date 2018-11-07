5e110f7e52a956c536258cb674de56eec3c19efe
Alessandro Mandelli <alessandro.mandelli@costergroup.eu>
2018-11-07 19:36:00 +0100

    Commit sistemazione

--- WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,17 @@ Partial Class Form1
     'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
     'Può essere modificata in Progettazione Windows Form.  
     'Non modificarla mediante l'editor del codice.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.MultiPanelSwitch1 = New MultiPanelSwitch.MultiPanelSwitch()
         Me.MultiPanelSwitch2 = New MultiPanelSwitch.MultiPanelSwitch()
         Me.MultiPanelSwitch3 = New MultiPanelSwitch.MultiPanelSwitch()
         Me.MultiPanelSwitch4 = New MultiPanelSwitch.MultiPanelSwitch()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MultiPanelSwitch5 = New MultiPanelSwitch.MultiPanelSwitch()
+        Me.MultiPanelSwitch6 = New MultiPanelSwitch.MultiPanelSwitch()
+        Me.MultiGenPoint3TemperatureWeeklyStrip1 = New MultiGenPoint3TemperatureWeeklyStrip.MultiGenPoint3TemperatureWeeklyStrip()
+        Me.MultiGenPoint3TemperatureWeeklyStrip2 = New MultiGenPoint3TemperatureWeeklyStrip.MultiGenPoint3TemperatureWeeklyStrip()
         Me.SuspendLayout()
         '
         'MultiPanelSwitch1
@@ -51,7 +56,7 @@ Partial Class Form1
         Me.MultiPanelSwitch2.Location = New System.Drawing.Point(145, 44)
         Me.MultiPanelSwitch2.Name = "MultiPanelSwitch2"
         Me.MultiPanelSwitch2.positions = 3
-        Me.MultiPanelSwitch2.semaphorColor = System.Drawing.Color.Yellow
+        Me.MultiPanelSwitch2.semaphorColor = System.Drawing.Color.Black
         Me.MultiPanelSwitch2.semaphortext = "Text"
         Me.MultiPanelSwitch2.Size = New System.Drawing.Size(64, 200)
         Me.MultiPanelSwitch2.TabIndex = 0
@@ -79,16 +84,88 @@ Partial Class Form1
         Me.MultiPanelSwitch4.Location = New System.Drawing.Point(285, 44)
         Me.MultiPanelSwitch4.Name = "MultiPanelSwitch4"
         Me.MultiPanelSwitch4.positions = 2
-        Me.MultiPanelSwitch4.semaphorColor = System.Drawing.Color.Red
+        Me.MultiPanelSwitch4.semaphorColor = System.Drawing.Color.Black
         Me.MultiPanelSwitch4.semaphortext = "Text"
         Me.MultiPanelSwitch4.Size = New System.Drawing.Size(64, 200)
         Me.MultiPanelSwitch4.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(474, 272)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'MultiPanelSwitch5
+        '
+        Me.MultiPanelSwitch5.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MultiPanelSwitch5.isSemaphorBlinking = False
+        Me.MultiPanelSwitch5.isSemaphorVisible = False
+        Me.MultiPanelSwitch5.lbltext = "Chrono 1"
+        Me.MultiPanelSwitch5.Location = New System.Drawing.Point(387, 44)
+        Me.MultiPanelSwitch5.Name = "MultiPanelSwitch5"
+        Me.MultiPanelSwitch5.positions = 3
+        Me.MultiPanelSwitch5.semaphorColor = System.Drawing.Color.Yellow
+        Me.MultiPanelSwitch5.semaphortext = "Text"
+        Me.MultiPanelSwitch5.Size = New System.Drawing.Size(64, 100)
+        Me.MultiPanelSwitch5.TabIndex = 2
+        '
+        'MultiPanelSwitch6
+        '
+        Me.MultiPanelSwitch6.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MultiPanelSwitch6.isSemaphorBlinking = False
+        Me.MultiPanelSwitch6.isSemaphorVisible = False
+        Me.MultiPanelSwitch6.lbltext = "Text"
+        Me.MultiPanelSwitch6.Location = New System.Drawing.Point(457, 44)
+        Me.MultiPanelSwitch6.Name = "MultiPanelSwitch6"
+        Me.MultiPanelSwitch6.positions = 3
+        Me.MultiPanelSwitch6.semaphorColor = System.Drawing.Color.Yellow
+        Me.MultiPanelSwitch6.semaphortext = "Text"
+        Me.MultiPanelSwitch6.Size = New System.Drawing.Size(64, 100)
+        Me.MultiPanelSwitch6.TabIndex = 3
+        '
+        'MultiGenPoint3TemperatureWeeklyStrip1
+        '
+        Me.MultiGenPoint3TemperatureWeeklyStrip1.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MultiGenPoint3TemperatureWeeklyStrip1.isCooling = False
+        Me.MultiGenPoint3TemperatureWeeklyStrip1.isEco = False
+        Me.MultiGenPoint3TemperatureWeeklyStrip1.isHeating = True
+        Me.MultiGenPoint3TemperatureWeeklyStrip1.isManual = False
+        Me.MultiGenPoint3TemperatureWeeklyStrip1.isOff = True
+        Me.MultiGenPoint3TemperatureWeeklyStrip1.Location = New System.Drawing.Point(595, 44)
+        Me.MultiGenPoint3TemperatureWeeklyStrip1.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.MultiGenPoint3TemperatureWeeklyStrip1.Name = "MultiGenPoint3TemperatureWeeklyStrip1"
+        Me.MultiGenPoint3TemperatureWeeklyStrip1.Size = New System.Drawing.Size(294, 130)
+        Me.MultiGenPoint3TemperatureWeeklyStrip1.TabIndex = 4
+        Me.MultiGenPoint3TemperatureWeeklyStrip1.temperature = 23.4R
+        '
+        'MultiGenPoint3TemperatureWeeklyStrip2
+        '
+        Me.MultiGenPoint3TemperatureWeeklyStrip2.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MultiGenPoint3TemperatureWeeklyStrip2.isCooling = False
+        Me.MultiGenPoint3TemperatureWeeklyStrip2.isEco = False
+        Me.MultiGenPoint3TemperatureWeeklyStrip2.isHeating = True
+        Me.MultiGenPoint3TemperatureWeeklyStrip2.isManual = False
+        Me.MultiGenPoint3TemperatureWeeklyStrip2.isOff = True
+        Me.MultiGenPoint3TemperatureWeeklyStrip2.Location = New System.Drawing.Point(595, 178)
+        Me.MultiGenPoint3TemperatureWeeklyStrip2.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.MultiGenPoint3TemperatureWeeklyStrip2.Name = "MultiGenPoint3TemperatureWeeklyStrip2"
+        Me.MultiGenPoint3TemperatureWeeklyStrip2.Size = New System.Drawing.Size(294, 130)
+        Me.MultiGenPoint3TemperatureWeeklyStrip2.TabIndex = 4
+        Me.MultiGenPoint3TemperatureWeeklyStrip2.temperature = 23.4R
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(964, 517)
+        Me.Controls.Add(Me.MultiGenPoint3TemperatureWeeklyStrip2)
+        Me.Controls.Add(Me.MultiGenPoint3TemperatureWeeklyStrip1)
+        Me.Controls.Add(Me.MultiPanelSwitch6)
+        Me.Controls.Add(Me.MultiPanelSwitch5)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.MultiPanelSwitch4)
         Me.Controls.Add(Me.MultiPanelSwitch3)
         Me.Controls.Add(Me.MultiPanelSwitch2)
@@ -103,4 +180,9 @@ Partial Class Form1
     Friend WithEvents MultiPanelSwitch2 As MultiPanelSwitch.MultiPanelSwitch
     Friend WithEvents MultiPanelSwitch3 As MultiPanelSwitch.MultiPanelSwitch
     Friend WithEvents MultiPanelSwitch4 As MultiPanelSwitch.MultiPanelSwitch
+    Friend WithEvents Button1 As Button
+    Friend WithEvents MultiPanelSwitch5 As MultiPanelSwitch.MultiPanelSwitch
+    Friend WithEvents MultiPanelSwitch6 As MultiPanelSwitch.MultiPanelSwitch
+    Friend WithEvents MultiGenPoint3TemperatureWeeklyStrip1 As MultiGenPoint3TemperatureWeeklyStrip.MultiGenPoint3TemperatureWeeklyStrip
+    Friend WithEvents MultiGenPoint3TemperatureWeeklyStrip2 As MultiGenPoint3TemperatureWeeklyStrip.MultiGenPoint3TemperatureWeeklyStrip
 End Class
--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,3 +1,34 @@
 ﻿Public Class Form1
+    Dim i As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If i Mod 2 = 0 Then
+            MultiPanelSwitch4.semaphorColor = Color.Red
+            MultiPanelSwitch4.isSemaphorVisible = True
+            MultiPanelSwitch4.isSemaphorBlinking = True
+        Else
+            MultiPanelSwitch4.semaphorColor = Color.Black
+            MultiPanelSwitch4.isSemaphorVisible = True
+            MultiPanelSwitch4.isSemaphorBlinking = False
+        End If
+        i += 1
+
+
+    End Sub
+
+    Private Sub MultiPanelSwitch5_(sender As Object, e As EventArgs) Handles MultiPanelSwitch5.Click
+        If MultiPanelSwitch5.selectedPosition = 0 Then
+            MultiGenPoint3TemperatureWeeklyStrip1.isOff = True
+        Else
+            MultiGenPoint3TemperatureWeeklyStrip1.isOff = False
+        End If
+    End Sub
+    Private Sub MultiPanelSwitch6_(sender As Object, e As EventArgs) Handles MultiPanelSwitch6.Click
+        If MultiPanelSwitch6.selectedPosition = 0 Then
+            MultiGenPoint3TemperatureWeeklyStrip2.isOff = True
+        Else
+            MultiGenPoint3TemperatureWeeklyStrip2.isOff = False
+        End If
+    End Sub
 
 End Class
